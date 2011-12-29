b8a6a2358c78d5341ed25aba18df7e7634ba429a
jbsheblak@gmail.com <jbsheblak@gmail.com@930bd532-91f0-56ef-8095-274bc0bb591a>
2011-12-29 08:21:08 +0000

    Enemy now flies in a downwardly destructive path

--- OuterSpaceCathedral/OuterSpaceCathedral/Enemy.cs
+++ OuterSpaceCathedral/OuterSpaceCathedral/Enemy.cs
@@ -9,11 +9,29 @@ namespace OuterSpaceCathedral
     {
         private const int skSpriteWidth  = 16;
         private const int skSpriteHeight = 16;
+        
+        private Vector2 mVelocity = Vector2.Zero;
 
-        public Enemy(Vector2 initialPosition)
+        public Enemy(Vector2 initialPosition, Vector2 velocity)
         {
-            sourceRectangle = new Rectangle(0 , 0, skSpriteWidth, skSpriteHeight);
+            sourceRectangle = new Rectangle(0, 0, skSpriteWidth, skSpriteHeight);
             position = initialPosition;
+            mVelocity = velocity;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            position += (mVelocity * deltaTime);
+
+            RemoveIfOffscreen();
+        }
+
+        private void RemoveIfOffscreen()
+        {
+            if ( !GameConstants.RenderTargetRect.Intersects(PositionRectangle) )
+            {
+                RemoveObject();
+            }
         }
     }
 }

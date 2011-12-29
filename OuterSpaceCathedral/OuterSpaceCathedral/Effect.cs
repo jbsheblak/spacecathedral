8b38cbb5b4702240fae10fd4f97cba400cb10693
jbsheblak@gmail.com <jbsheblak@gmail.com@930bd532-91f0-56ef-8095-274bc0bb591a>
2011-12-29 09:22:21 +0000

    Adding animation frame manager. Added Effect class.

---
+++ OuterSpaceCathedral/OuterSpaceCathedral/Effect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OuterSpaceCathedral
+{
+    internal class Effect : GameObject
+    {
+        private const int skSpriteWidth  = 32;
+        private const int skSpriteHeight = 32;
+
+        private AnimFrameManager mAnimMgr = null;
+        private float            mLifeTime = 0;
+        
+        public Effect(Vector2 initialPosition)
+        {
+            List<Rectangle> animFrames = new List<Rectangle>()
+            {
+                GameConstants.CalcRectFor32x32Sprite(0, 2),
+                GameConstants.CalcRectFor32x32Sprite(1, 2),
+            };
+
+            mAnimMgr = new AnimFrameManager(1/10.0f, animFrames);       
+            mLifeTime = 4.0f;
+
+            sourceRectangle = mAnimMgr.FrameRectangle;
+            position = initialPosition;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            mAnimMgr.Update(deltaTime);
+            sourceRectangle = mAnimMgr.FrameRectangle;
+
+            mLifeTime = Math.Max(0, mLifeTime - deltaTime);
+            if ( mLifeTime == 0.0f )
+            {
+                RemoveObject();
+            }
+        }
+    }
+}

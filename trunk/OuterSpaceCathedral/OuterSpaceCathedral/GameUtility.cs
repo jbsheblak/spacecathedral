4b5499e4569b6be88ac7bdf7529f9c711b3b5333
rickst@gmail.com <rickst@gmail.com@930bd532-91f0-56ef-8095-274bc0bb591a>
2011-12-30 22:05:41 +0000

    Added GameUtility with a Random.  Added UnclutteredLine.  Added new enemy types.

---
+++ trunk/OuterSpaceCathedral/OuterSpaceCathedral/GameUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OuterSpaceCathedral
+{
+    static internal class GameUtility
+    {
+        static GameUtility()
+        {
+            Random = new Random();
+        }
+
+        /// <summary>
+        /// Rectangle representing render target dimensions.
+        /// </summary>
+        public static Random Random    { get; private set; }
+    }
+}

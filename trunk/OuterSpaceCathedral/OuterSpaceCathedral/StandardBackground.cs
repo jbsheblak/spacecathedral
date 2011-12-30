962dea986568243a90ed3ef3d47b1c19663ce3b5
rickst@gmail.com <rickst@gmail.com@930bd532-91f0-56ef-8095-274bc0bb591a>
2011-12-30 04:56:04 +0000

    checking in StandardBackground.cs

---
+++ trunk/OuterSpaceCathedral/OuterSpaceCathedral/StandardBackground.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OuterSpaceCathedral
+{
+    class StandardBackground : Background
+    {
+
+    }
+}

beeb83fa90c29769daac6e30b8488a050c3dedd4
rickst@gmail.com <rickst@gmail.com@930bd532-91f0-56ef-8095-274bc0bb591a>
2011-12-31 10:55:42 +0000

    An ocean is delivered

--- OuterSpaceCathedral/OuterSpaceCathedral/Level.cs
+++ OuterSpaceCathedral/OuterSpaceCathedral/Level.cs
@@ -896,6 +896,7 @@ namespace OuterSpaceCathedral
                 case "city_clouds_background":          return new Rectangle(0, 1024 + 300, 960, 270);
                 case "city_clouds_background2":         return new Rectangle(0, 1024 + 350, 960, 270);
                 case "city_background":                 return new Rectangle(0, 1024 + 270 * 2, 960, 270);
+                case "ocean_background":                return new Rectangle(0, 610, 960, 270);
                 case "purple_clouds_background":        return new Rectangle(960, 1564, 960, 270);
                 case "stars_background":                return new Rectangle(0, 880, 960, 270);
                 case "clouds_foreground":               return new Rectangle(0, 1024 + 150, 960, 270);

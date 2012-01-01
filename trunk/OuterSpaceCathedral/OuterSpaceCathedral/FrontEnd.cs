a173efa22b736e176ac66539903f749f9cf3cdda
jbsheblak@gmail.com <jbsheblak@gmail.com@930bd532-91f0-56ef-8095-274bc0bb591a>
2012-01-01 17:40:27 +0000

    Fixing spelling of orchestra

--- trunk/OuterSpaceCathedral/OuterSpaceCathedral/FrontEnd.cs
+++ trunk/OuterSpaceCathedral/OuterSpaceCathedral/FrontEnd.cs
@@ -47,7 +47,7 @@ namespace OuterSpaceCathedral
         #endif
 
             // level unlocks
-            mLevelEntries.Add( new LevelEntry("Ocean Ochestra",     "content\\levels\\Level1.xml", "22:00:00",   unlockedByDefault) );
+            mLevelEntries.Add( new LevelEntry("Ocean Orchestra",    "content\\levels\\Level1.xml", "22:00:00",   unlockedByDefault) );
             mLevelEntries.Add( new LevelEntry("Cathedral 300",      "content\\levels\\Level2.xml", "23:00:00",   unlockedByDefault) );
             mLevelEntries.Add( new LevelEntry("SECRET",             "content\\levels\\Level3.xml", "23:58:00",   unlockedByDefault) );
 
